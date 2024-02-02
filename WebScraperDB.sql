464e67eecfcfd4c845ba849c7165112277492cc8
DavidHP <maritotupapasito@gmail.com>
2024-02-01 19:33:12 -0600

    Second commit technically

--- WebScraperDB.sql
+++
@@ -1,5 +0,0 @@
--- Just the Script of SQL to create the Database of our project.
-CREATE DATABASE WebScraperDB;
--- Here we'll be creating many tables of the URLs of each product by the first code in python,
--- then the URLs that would be already used by the second code in python,
--- and lastly the data scraped by this same second code.
